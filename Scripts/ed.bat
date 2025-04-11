b895bb763dd3ccc7f13faca03de4babbc41a3894
Aravind Anugonda <94236623+aravindanugonda@users.noreply.github.com>
2025-04-11 00:30:44 -0400

    Update ed.bat

--- Scripts/ed.bat
+++ Scripts/ed.bat
@@ -2,14 +2,9 @@
 setlocal  
 
 echo ===== ENTERPRISE DEVELOPER ENVIRONMENT SETUP =====  
-echo Input parameter: %1  
 
 echo Calling environment setup...  
-set "setupPath=C:\PROGRA~2\Micro Focus\Enterprise Developer\SetupEnv.bat"  
-
-echo Calling SetupEnv.bat using: %setupPath% 32  
-call "%setupPath%" 32
-
+call Scripts\env.bat  
 echo Setup completed with return code: %ERRORLEVEL%  
 
 if defined COBDIR (  
@@ -20,4 +15,4 @@ if defined COBDIR (
   exit /b 1  
 )  
 
-exit /b 0
+exit /b 0  
