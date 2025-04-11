be29db0ae540a95c00aebe9a49c18b398d87184f
Aravind Anugonda <94236623+aravindanugonda@users.noreply.github.com>
2025-04-10 22:13:37 -0400

    Update ed.bat

--- Scripts/ed.bat
+++ Scripts/ed.bat
@@ -8,11 +8,13 @@ if "%1"=="64" set ARCH=64
 
 echo Setting up Enterprise Developer %ARCH%-bit environment...  
 
-set MF_ROOT=%ProgramFiles(x86)%\Micro Focus\Enterprise Developer  
-echo MF_ROOT: !MF_ROOT!  
+rem Use short path format to avoid parentheses problems  
+for /f "delims=" %%i in ('dir /x /b "%ProgramFiles(x86)%\Micro Focus\Enterprise Developer\SetupEnv.bat"') do set SETUP_PATH=%ProgramFiles(x86)%\MICROS~1\ENTERP~1\%%i  
 
-echo Calling SetupEnv.bat...  
-call "!MF_ROOT!\SetupEnv.bat" %ARCH%  
+echo Using setup path: !SETUP_PATH!  
+
+rem Call SetupEnv using the short path  
+call !SETUP_PATH! %ARCH%  
 set SETUP_RESULT=%ERRORLEVEL%  
 
 echo SetupEnv.bat completed with return code: %SETUP_RESULT%  
@@ -25,7 +27,4 @@ if defined COBDIR (
   exit /b 1  
 )  
 
-echo PATH: %PATH%  
-echo.  
-echo Environment setup complete.  
 exit /b 0
